ec73b9d7852b8db4cd68913358f5428d45e47f32
srijan <srijantripathi55@gmail.com>
2026-05-21 22:22:05 +0530

    render updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
     <Routes>
-      <Route path='/login' element={<Login/>}/>
+      <Route path='/' element={<Login/>}/>
       <Route path='/register' element={<Register/>}/>
       <Route path='/home' element={<Landing/>}/>
     </Routes>
